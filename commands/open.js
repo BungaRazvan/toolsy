48e13aa02235d5aa33d5692714b2bb57c4d52a1c
Bunga Razvan <razvanbunga@gmail.com>
2020-05-31 13:06:15 +0100

    polish .open command

--- commands/open.js
+++ commands/open.js
@@ -1,5 +1,6 @@
 const { Client, RichEmbed } = require("discord.js");
 const colors = require("../json/colors.json");
+const { Op } = require("sequelize");
 const {
   Skins,
   Knives,
@@ -10,26 +11,62 @@ const {
 
 module.exports.run = async (bot, message, args) => {
   const authorId = message.author.id;
-
   let container = args.join(" ");
 
+  const quality = {
+    Mil: 600,
+    Restricted: 300,
+    Classified: 150,
+    Covert: 2,
+    Knife: 1,
+  };
+
+  function get(input) {
+    var array = [];
+    for (var quality in input) {
+      if (input.hasOwnProperty(quality)) {
+        for (var i = 0; i < input[quality]; i++) {
+          array.push(quality);
+        }
+      }
+    }
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
   if (container) {
-    cases = await Cases.findOne({
-      where: { case_name: container },
-      include: [Skins, Knives],
-    });
+    const picked = get(quality);
+
+    if (picked !== "Knife") {
+      cases = await Cases.findOne({
+        where: { case_name: container },
+        include: {
+          model: Skins,
+          where: { skin_quality: { [Op.like]: `%${picked}%` } },
+        },
+      });
+
+      const randomSkin =
+        cases.skins[Math.floor(Math.random() * cases.skins.length)].skin_image;
+
+      message.channel.send(`${message.author} `, { files: [`${randomSkin}`] });
+    } else {
+      cases = await Cases.findOne({
+        where: { case_name: container },
+        include: {
+          model: Knives,
+        },
+      });
+
+      const randomSkin =
+        cases.knives[Math.floor(Math.random() * cases.knives.length)]
+          .knife_image;
+
+      message.channel.send(`${message.author} `, { files: randomSkin });
+    }
 
     if (!cases) {
       message.channel.send("No skins!! Work in progress!!");
     }
-
-    const randomSkin =
-      cases.skins[Math.floor(Math.random() * cases.skins.length)].skin_image;
-
-    console.log(randomSkin);
-    const attachment = new RichEmbed().setThumbnail(`${randomSkin}`);
-
-    message.channel.send(`${message.author} ${randomSkin}`);
   } else {
     message.channel.send("You have to specifie a case");
   }
@@ -40,5 +77,3 @@ module.exports.config = {
   description: "Open a case",
   usage: "!open",
 };
-
-//   file: cases.skins[Math.floor(Math.random() * cases.skins.length)],
