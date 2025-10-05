cc8672b66ed6bf6315113d62b16c0ad2dddfe62d
Bunga Razvan <razvanbunga@gmail.com>
2025-10-05 14:07:32 +0100

    more docker fixes

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ docker rm toolsy -f || true
 docker load -i toolsy.tar
 
 docker run -d \
-    --name toolsy
+    --name toolsy \
     --env-file /home/pi/projects/toolsy.env \
     -v /home/pi/projects/toolsy/database.sqlite:/app/database.sqlite \
     toolsy:latest
