6021a0e1ebfbb944fc1ae31ac0c34af4df0ab0ab
Bunga Razvan <razvanbunga@gmail.com>
2026-04-03 16:36:49 +0100

    update discord.js and build scripts.

--- run.sh
+++ run.sh
@@ -8,7 +8,6 @@ docker load -i toolsy.tar
 docker run -d \
     --name toolsy \
     --env-file /home/pi/projects/toolsy/.env \
-    -v /home/pi/projects/toolsy/database.sqlite:/app/database.sqlite \
     toolsy:latest
 
 docker exec -it toolsy npm run dev
